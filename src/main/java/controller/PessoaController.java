0b252a8236c493651dfc95abb89f0c0ec499b57c
jeferson_r <jeffgo4@gmail.com>
2019-02-19 16:56:36 -0300

    inicio

--- src/main/java/controller/PessoaController.java
+++ src/main/java/controller/PessoaController.java
@@ -1,77 +1,79 @@
 package controller;
 
 import modelo.Pessoa;
+import repositorio.PessoaDAO;
 
 import javax.annotation.PostConstruct;
-import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.context.SessionScoped;
-import javax.faces.view.ViewScoped;
+import javax.inject.Inject;
 import javax.inject.Named;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
-@Named //gerenciado pelo JSF
-//@SessionScoped //bean do cdi com escopo de sessão
-@ViewScoped
+@SessionScoped
+@Named
 public class PessoaController implements Serializable {
+    private Pessoa pessoaForm;
+    private List<Pessoa> pessoas;// = new ArrayList<>();
+    private Pessoa pessoaSelecionada;
 
-    private String nome;
-    private String sexo;
-    private String endereco;
-
-    private List<Pessoa> pessoas;
+    @Inject
+    private PessoaDAO pessoaDAO;
 
     @PostConstruct
-    private void init(){
-        pessoas = new ArrayList<>();
+    private void innit (){
+        pessoaForm = new Pessoa();
+        pessoas = pessoaDAO.consultar();
     }
 
     public void cadastrar(){
-        Pessoa pessoa = new Pessoa();
-        pessoa.setNome(nome);
-        pessoa.setEndereco(endereco);
-        pessoa.setSexo(sexo);
-        pessoas.add(pessoa);
-        //limpar();
+        //pessoas.add(pessoaForm);
+        pessoaDAO.salvar(pessoaForm);
+        limpar();
+        atualizarTabela();
     }
 
-    public void setPessoas(List<Pessoa> pessoas) {
-        this.pessoas = pessoas;
+    public void atualizarTabela(){
+        
+    }
+
+    public void limpar(){
+        this.pessoaForm = new Pessoa();
+    }
+
+    public void excluir(){
+        this.pessoas.remove(pessoaForm);
+    }
+
+    public void aoSelecionar(){
+        this.pessoaForm = pessoaSelecionada;
+    }
+
+    public void aoDesselecionar(){
+        limpar();
     }
 
     public List<Pessoa> getPessoas() {
         return pessoas;
     }
-
-    public void limpar(){
-        setNome(null);
-        setEndereco(null);
-        setSexo(null);
-        setPessoas(null);
-    }
-    public void setSexo(String sexo) {
-        this.sexo = sexo;
+    public void setPessoas(List<Pessoa> pessoas) {
+        this.pessoas = pessoas;
     }
 
-    public void setEndereco(String endereco) {
-        this.endereco = endereco;
+    public Pessoa getPessoaSelecionada() {
+        return pessoaSelecionada;
     }
 
-    public String getSexo() {
-        return sexo;
+    public void setPessoaSelecionada(Pessoa pessoaSelecionada) {
+        this.pessoaSelecionada = pessoaSelecionada;
     }
 
-    public String getEndereco() {
-        return endereco;
+    public Pessoa getPessoaForm() {
+        return pessoaForm;
     }
 
-    public void setNome(String nome) {
-        this.nome = nome;
+    public void setPessoaForm(Pessoa pessoaForm) {
+        this.pessoaForm = pessoaForm;
     }
-
-    public String getNome() {
-        return nome;
-    }
-
 }
